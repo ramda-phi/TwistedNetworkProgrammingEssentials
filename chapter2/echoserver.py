94924215175d57ab6ea8dc1609fc8f3d1c843648
yago <kennedy.icu@gmail.com>
2012-11-20 07:57:44 +0900

    2012.11.20 7:57

---
+++ chapter2/echoserver.py
@@ -0,0 +1,25 @@
+from twisted.internet import reactor, protocol
+from twisted.protocols import basic
+
+
+class EchoProtocol(basic.LineReceiver):
+    def connectionMade(self):
+        print "Connection from", self.transport.getPeer().host
+
+    def lineReceived(self, line):
+        if line == 'quit':
+            self.sendLine("Goodbye.")
+            self.transport.loseConnection()
+        else:
+            self.sendLine("You said: " + line)
+
+
+class EchoServerFactory(protocol.ServerFactory):
+    protocol = EchoProtocol
+
+
+if __name__ == '__main__':
+    port = 5001
+    reactor.listenTCP(port, EchoServerFactory())
+    print "Server rybbubg, press ctrl-C to stop."
+    reactor.run()
